b7c708b9c3fc08353784a823a4aafd910c836a8c
daniel <dani.martinez.3.fernandez@gmail.com>
2017-11-23 13:19:09 +0100

    Version 2.0

--- src/calculadoras/Operaciones.java
+++ src/calculadoras/Operaciones.java
@@ -11,17 +11,35 @@ public class Operaciones {
 		int resultado;
 		
 		Scanner in = new Scanner(System.in);
-		System.out.println("Introduce el primer número.");
-		primero = in.nextInt();
-		System.out.println("Introduce el segundo número.");
-		segundo = in.nextInt();
+		
 		System.out.println("Escribe 1 para sumar, 2, para restar, "
 				+ "3 para multiplicar y 4 para dividir");
 		operacion = in.nextInt();
-		if (operacion == 1) {
+		
+		System.out.println("Introduce el primer número.");
+		primero = in.nextInt();
+		
+		System.out.println("Introduce el segundo número.");
+		segundo = in.nextInt();
+		
+		switch (operacion)
+		{
+		case 1:
+		{
 			resultado = primero + segundo;
 			System.out.println("El resultado de la suma es: " + resultado);
+			break;
 		}
+		
+		case 2:
+		{
+			resultado = primero - segundo;
+			System.out.println("El resultado de la resta es: " + resultado);
+			break;
+		}
+		
+		}
+		in.close();
 
 	}
 
